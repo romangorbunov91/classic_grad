555af720831242a313b05c70b2b0e492a7bcfa14
Roman Gorbunov <romangorbunov91@yandex.ru>
2025-08-11 10:38:14 +0700

    v0.7

--- Functions/optimization.py
+++ Functions/optimization.py
@@ -1,18 +1,18 @@
-# version 0.1 by romangorbunov91
-# 08-Aug-2025
+# version 0.2 by romangorbunov91
+# 11-Aug-2025
 import numpy as np
-from IPython.display import clear_output
 
 # Классический градиентный спуск.
-def classic_grad_descent(loss_func, grad_func, x, learning_rate, tolerance):
+def classic_grad_descent(loss_func, grad_func, x_init, learning_rate, tolerance, printoutput):
     
-    iteration_max = 200000
+    iteration_max = 10000000
     
     func_counter = 0
     grad_counter = 0
     
     losses = []
     
+    x = x_init.copy()
     for i in range(iteration_max):
         grad = grad_func(x)
         grad_counter += 1
@@ -26,23 +26,21 @@ def classic_grad_descent(loss_func, grad_func, x, learning_rate, tolerance):
         losses.append(loss)
         
         x_norm = np.linalg.norm((x - x_prev), ord=None, axis=None)
-        
-        if i % 100 == 0:
-            clear_output(wait=True)
-            print('Iteration:', i)
-            print('x-values:', np.round(x,4))
-            print('x-norm:', np.round(x_norm,4))
-            print('Gradient:', np.round(grad,4))
-            print('Loss:', np.round(loss,4))
-            
-        if (x_norm < tolerance):
-            print('Iteration:', i)
-            print('x-values:', np.round(x,4))
-            print('x-norm:', np.round(x_norm,4))
-            print('Gradient:', np.round(grad,4))
-            print('Loss:', np.round(loss,4))
-            break
-    
-    iter_final = i
+        grad_norm = np.linalg.norm(grad, ord=None, axis=None)
 
-    return x, losses, iter_final, func_counter, grad_counter
+        if (grad_norm < tolerance):
+            if printoutput:
+                print('Iteration:', i)
+                print('x-values:', np.round(x,4))
+                print('x-norm:', np.round(x_norm,4))
+                print('Gradient norm:', np.round(grad_norm,4))
+                print('Loss:', np.round(loss,4))
+            break
+    if printoutput:
+        print('Iteration:', i)
+        print('x-values:', np.round(x,4))
+        print('x-norm:', np.round(x_norm,4))
+        print('Gradient norm:', np.round(grad_norm,4))
+        print('Loss:', np.round(loss,4))
+
+    return x, losses, i+1, func_counter, grad_counter
