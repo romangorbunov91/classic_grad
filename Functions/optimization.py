ba3e370a5d9de20b4245b0df8ca9ff3297252355
Roman Gorbunov <romangorbunov91@yandex.ru>
2025-08-15 10:31:45 +0700

    v0.1.1

--- Functions/optimization.py
+++ Functions/optimization.py
@@ -1,5 +1,5 @@
-# version 0.3 by romangorbunov91
-# 12-Aug-2025
+# version 0.4 by romangorbunov91
+# 15-Aug-2025
 import numpy as np
 
 # Классический градиентный спуск.
@@ -21,6 +21,51 @@ def classic_grad_descent(loss_func, grad_func, x_init, learning_rate, tolerance,
         x_prev = x.copy()
         
         x -= learning_rate * grad
+        
+        loss = loss_func(x)
+        func_counter += 1
+        losses.append(loss)
+        
+        x_norm = np.linalg.norm((x - x_prev), ord=None, axis=None)
+        grad_norm = np.linalg.norm(grad, ord=None, axis=None)
+
+        if (grad_norm < tolerance):
+            if printoutput:
+                print('Iteration:', i)
+                print('x-values:', np.round(x,4))
+                print('x-norm:', np.round(x_norm,4))
+                print('Gradient norm:', np.round(grad_norm,4))
+                print('Loss:', np.round(loss,4))
+            break
+    if printoutput:
+        print('Iteration:', i)
+        print('x-values:', np.round(x,4))
+        print('x-norm:', np.round(x_norm,4))
+        print('Gradient norm:', np.round(grad_norm,4))
+        print('Loss:', np.round(loss,4))
+
+    return x, losses, i+1, func_counter, grad_counter
+
+
+# Градиентный спуск с дроблением шага по условию Армихо.
+def armijo_grad_descent(loss_func, grad_func, x_init, learning_rate_init, tolerance, printoutput):
+    # both 'x_init' and 'learning_rate_init' must be np.array([val1, val2]).
+    # 'printoutput' is BOOL.
+    iteration_max = 10000000
+    
+    func_counter = 0
+    grad_counter = 0
+    
+    losses = []
+    
+    x = x_init.copy()
+    for i in range(iteration_max):
+        grad = grad_func(x)
+        grad_counter += 1
+        
+        x_prev = x.copy()
+        
+        x -= learning_rate_init * grad
         
         loss = loss_func(x)
         func_counter += 1
