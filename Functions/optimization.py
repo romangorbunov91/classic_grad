fa66f0572ecfc3188f9a8415cd163b3dc2b91155
Roman Gorbunov <romangorbunov91@yandex.ru>
2025-08-18 10:31:43 +0700

    v0.1.3

--- Functions/optimization.py
+++ Functions/optimization.py
@@ -48,22 +48,28 @@ def classic_grad_descent(loss_func, grad_func, x_init, learning_rate, tolerance,
 
 
 # Градиентный спуск с дроблением шага по условию Армихо.
-def armijo_grad_descent(loss_func, grad_func, x_init, learning_rate_init, lr_multiplier, tolerance, printoutput):
+def armijo_grad_descent(loss_func, grad_func, x_init, lr_multiplier, lr_coeff, tolerance, printoutput):
     # both 'x_init' and 'learning_rate_init' must be np.array([val1, val2]).
     # 'printoutput' is BOOL.
     iteration_max = 10000000
     
-    func_counter = 0
-    grad_counter = 0
-    
     losses = []
     
     x = x_init.copy()
-    learning_rate = learning_rate_init.copy()
+    loss = loss_func(x)
+    grad = grad_func(x)
+    func_counter = 1
+    grad_counter = 1
+    
+    grad_norm = np.linalg.norm(grad, ord=None, axis=None)
     
     for i in range(iteration_max):
-        grad = grad_func(x)
-        grad_counter += 1
+        learning_rate = np.array([1.0, 1.0])
+        while (loss - loss_func(x - learning_rate * grad)) < lr_coeff * learning_rate[0] * grad_norm**2:
+            func_counter += 1
+            learning_rate *= lr_multiplier
+        # One more increment.
+        func_counter += 1
         
         x_prev = x.copy()
         x -= learning_rate * grad
@@ -71,17 +77,9 @@ def armijo_grad_descent(loss_func, grad_func, x_init, learning_rate_init, lr_mul
         loss = loss_func(x)
         func_counter += 1
         losses.append(loss)
-        learning_rate *= lr_multiplier
-        
-        if i != 0:
-            if (losses[-2] - losses[-1]) < tolerance * learning_rate[0] * grad_norm**2:
-                if printoutput:
-                    print('Iteration:', i)
-                    print('x-values:', np.round(x,4))
-                    print('x-norm:', np.round(x_norm,4))
-                    print('Gradient norm:', np.round(grad_norm,4))
-                    print('Loss:', np.round(loss,4))
-                break
+
+        grad = grad_func(x)
+        grad_counter += 1
         
         # New norm-values.
         x_norm = np.linalg.norm((x - x_prev), ord=None, axis=None)
