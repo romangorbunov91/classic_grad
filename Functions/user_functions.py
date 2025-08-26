512f0924cfe75a6f90569248bcc5a5a170fc75ed
Roman Gorbunov <romangorbunov91@yandex.ru>
2025-08-26 11:29:13 +0700

    v0.5.0
    Реализован генератор функций.

--- Functions/user_functions.py
+++ Functions/user_functions.py
@@ -1,6 +1,7 @@
-# version 0.2 by romangorbunov91
-# 19-Aug-2025
+# version 0.3 by romangorbunov91
+# 26-Aug-2025
 
+import numpy as np
 import re
 
 def update_readme_section(tbl_df, readme_path, tbl_name, section):
@@ -25,3 +26,40 @@ def update_readme_section(tbl_df, readme_path, tbl_name, section):
 
     with open(readme_path, 'w', encoding="utf-8-sig") as f:
         f.write(updated_content)
+
+        
+def funtion_generator(N, cond_number, seed=None):
+    # cond_number - condition number.
+    if seed is not None:
+        np.random.seed(seed)
+
+    if cond_number < 1:
+        raise ValueError('Condition number must be >= 1.')
+    
+    # Generate N random eigenvalues.
+    eigvals_float = [np.random.uniform(1+0.01, cond_number-0.01) for _ in range(N)]
+    eigvals_float[0] = 1.0
+    eigvals_float[-1] = cond_number
+    #eigvals_float = np.linspace(1.0, cond_number, N)
+    
+    # Generate random orthogonal matrix using QR-decomposition.
+    Q, _ = np.linalg.qr(np.random.randn(N, N))
+
+    # Build system matrix A.
+    A = Q @ np.diag(eigvals_float) @ Q.T
+    
+    # Generate random b-vector (N-dimensional).
+    b = np.random.randn(N)
+    
+    # Generate random constant.
+    c = np.random.uniform(-100.0, 100.0)
+    
+    # Finally, define function and its gradient.
+    
+    def func(x):
+        return x @ A @ x + b @ x + c
+
+    def grad_func(x):
+        return 2*A @ x + b
+
+    return A, b, c, func, grad_func
