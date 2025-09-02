524a64dd725ac7489e6536719b23d2a72d67d711
Roman Gorbunov <romangorbunov91@yandex.ru>
2025-09-02 15:43:19 +0700

    v0.5.1

--- Functions/user_functions.py
+++ Functions/user_functions.py
@@ -1,5 +1,5 @@
-# version 0.3 by romangorbunov91
-# 26-Aug-2025
+# version 1.0 by romangorbunov91
+# 02-Sep-2025
 
 import numpy as np
 import re
@@ -28,28 +28,29 @@ def update_readme_section(tbl_df, readme_path, tbl_name, section):
         f.write(updated_content)
 
         
-def funtion_generator(N, cond_number, seed=None):
-    # cond_number - condition number.
+def funtion_generator(M, cond_number, seed=None):
+    # M - (order).
+    # cond_number - condition number µ.
     if seed is not None:
         np.random.seed(seed)
 
     if cond_number < 1:
         raise ValueError('Condition number must be >= 1.')
     
-    # Generate N random eigenvalues.
-    eigvals_float = [np.random.uniform(1+0.01, cond_number-0.01) for _ in range(N)]
+    # Generate M random eigenvalues.
+    #eigvals_float = np.linspace(1.0, cond_number, M)
+    eigvals_float = [np.random.uniform(1+0.01, cond_number-0.01) for _ in range(M)]
     eigvals_float[0] = 1.0
     eigvals_float[-1] = cond_number
-    #eigvals_float = np.linspace(1.0, cond_number, N)
     
     # Generate random orthogonal matrix using QR-decomposition.
-    Q, _ = np.linalg.qr(np.random.randn(N, N))
+    Q, _ = np.linalg.qr(np.random.randn(M, M))
 
     # Build system matrix A.
     A = Q @ np.diag(eigvals_float) @ Q.T
     
-    # Generate random b-vector (N-dimensional).
-    b = np.random.randn(N)
+    # Generate random b-vector (M-dimensional).
+    b = np.random.randn(M)
     
     # Generate random constant.
     c = np.random.uniform(-100.0, 100.0)
